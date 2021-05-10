f8e6154ec0b9cf9f1bfd36e5d5af68ed4d7da65e
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-05-10 10:21:45 -0400

    00_cleanup.js is completed

---
+++ data/seeds/00_cleanup.js
@@ -0,0 +1,7 @@
+exports.seed = async function (knex) {
+    await knex("recipes").truncate();
+    await knex("ingredients").truncate();
+    await knex("recipe_ingredients").truncate();
+    await knex("users").truncate();
+    await knex("roles").truncate();
+  };
