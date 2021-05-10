4ecd13383105a2046983ef3c3b3270c273c52892
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-05-10 10:26:55 -0400

    index.js is completed

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,7 +1,3 @@
 exports.seed = async function (knex) {
-    await knex("recipes").truncate();
-    await knex("ingredients").truncate();
-    await knex("recipe_ingredients").truncate();
     await knex("users").truncate();
-    await knex("roles").truncate();
   };
