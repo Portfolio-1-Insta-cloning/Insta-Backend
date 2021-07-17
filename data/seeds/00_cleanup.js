fb8f559eab0a74896372211092888cfe9f882436
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2021-07-17 15:49:54 -0400

    seed files are ready to migrate

--- data/seeds/00_cleanup.js
+++ data/seeds/00_cleanup.js
@@ -1,4 +1,7 @@
 exports.seed = async function (knex) {
   await knex("instausers").del();
   await knex("roles").del();
+  await knex("courses").del();
+  await knex("title_description").del();
+  await knex("course_details").del();
 };
